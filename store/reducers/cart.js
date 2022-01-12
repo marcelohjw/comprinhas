285ada7b13978fe0f375e02a7420ae8f8de26076
Marcelo Júnior <marcelohjw@gmail.com>
2022-01-12 20:27:02 -0300

    Adding logic to Delete Items

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from "../actions/cart";
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart";
 import CartItem from "../../models/cart-item";
 
 const initialState = {
@@ -30,6 +30,31 @@ export default (state = initialState, action) => {
                 ...state,
                 items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
                 totalAmount: state.totalAmount + prodPrice
+            }
+        case REMOVE_FROM_CART:
+            const selectedCartItem = state.items[action.pid];
+            const currentQuantity = selectedCartItem.quantity;
+            let updatedCartItems;
+
+            if (currentQuantity > 1) {
+                // Need to reduce it, not erase
+                const updatedCartItem = new CartItem(
+                    selectedCartItem.quantity - 1,
+                    selectedCartItem.productPrice,
+                    selectedCartItem.productTitle,
+                    selectedCartItem.sum - selectedCartItem.productPrice
+                    );
+                    updatedCartItems = { ...state.items, [action.pid]: updatedCartItem };
+            } else {
+                // Erase it
+                updatedCartItems = { ...state.items };
+                delete updatedCartItems[action.pid];
+            }
+
+            return {
+                ...state,
+                items: updatedCartItems,
+                totalAmount: state.totalAmount - selectedCartItem.productPrice
             }
     }
     return state;
