62c6d6e50f6ae2ad0ef0f8b3aa7ddb2040fafe0b
Marcelo Junior <marcelohjw@gmail.com>
2022-02-17 08:52:17 -0300

    Handling User Input

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,28 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, ScrollView, Text, TextInput, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
 
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import CustomHeaderButton from '../../components/UI/HeaderButton'; 
 
 const EditProductScreen = props => {
+    const prodId = props.navigation.getParam('productId');
+    const editedProduct = useSelector(state => 
+        state.products.userProducts.find(prod => prod.id === prodId)
+    );
+    
+    const [title, setTitle] = useState(editedProduct ? editedProduct.title : '');
+    const [image, setImage] = useState(editedProduct ? editedProduct.imageUrl : '');
+    const [price, setPrice] = useState('');
+    const [description, setDescription] = useState(editedProduct ? editedProduct.description : '');
+
+  
+
     return (
         <ScrollView>
             <View style={styles.form}>
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Nome</Text>
-                    <TextInput style={styles.input}/>
+                    <TextInput 
+                        style={styles.input}
+                        value={title}
+                        onChange={text => setTitle(text)}
+                    />
                 </View>
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Imagem</Text>
-                    <TextInput style={styles.input}/>
+                    <TextInput 
+                        style={styles.input}
+                        value={image}
+                        onChange={text => setImage(text)}
+                    />
                 </View>
-                <View style={styles.formControl}>
-                    <Text style={styles.label}>Preço</Text>
-                    <TextInput style={styles.input}/>
-                </View>
+                {editedProduct ? null : (
+                    <View style={styles.formControl}>
+                        <Text style={styles.label}>Preço</Text>
+                        <TextInput 
+                            style={styles.input}
+                            value={price}
+                            onChange={text => setPrice(text)}
+                        />
+                    </View>)}
                 <View style={styles.formControl}>
                     <Text style={styles.label}>Descrição</Text>
-                    <TextInput style={styles.input}/>
+                    <TextInput 
+                        style={styles.input}
+                        value={description}
+                        onChange={text => setDescription(text)}
+                    />
                 </View>
             </View>
         </ScrollView>
