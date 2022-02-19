7286f3ff59d70ee7a520e10fda8b86b9193624a8
Marcelo Junior <marcelohjw@gmail.com>
2022-02-19 18:50:47 -0300

    Storing Orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,7 +1,34 @@
 export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
-    return { type: ADD_ORDER,
-            orderData: {items: cartItems, amount: totalAmount}
-    };
-}
+    return async dispatch => {
+        const date = new Date();
+        const response = await fetch('https://pilot-shop-fa4d0-default-rtdb.firebaseio.com/orders/u1.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                cartItems,
+                totalAmount,
+                date: date.toISOString()
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error('Algo deu errado!');
+        }
+
+        const resData = await response.json();
+
+
+        dispatch ({ type: ADD_ORDER,
+                orderData: {
+                    id: resData.name, 
+                    items: cartItems, 
+                    amount: totalAmount,
+                    date: date
+                }
+        });
+};
+};
