be833acc124f3371b06839f45818b25e41e116e6
Marcelo Junior <marcelohjw@gmail.com>
2022-02-19 21:51:34 -0300

    Fetching stored orders almost done and lil standalone works

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import { FlatList, Text, StyleSheet, View } from 'react-native';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { FlatList, View, ActivityIndicator, StyleSheet } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 
 import CustomHeaderButton from '../../components/UI/HeaderButton';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import OrderItem from '../../components/shop/OrderItem';
+import * as ordersActions from '../../store/actions/orders';
+import Colors from '../../constants/Colors';
 
 
 const OrdersScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
+
     const orders = useSelector(state => state.orders.orders);
+    const dispatch = useDispatch();
+
+    useEffect( async () => {
+        setIsLoading(true);
+        await dispatch(ordersActions.fetchOrders());
+        setIsLoading(false);
+    }, [dispatch])
+
+    if(isLoading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator size={'large'} color={Colors.primary}/>
+            </View>
+        );
+    }
 
     return (
         <View>
@@ -35,6 +54,14 @@ OrdersScreen.navigationOptions = navData => {
                         </HeaderButtons>
     };
 };
+
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 
 export default OrdersScreen;
--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,4 +1,38 @@
+import Order from "../../models/order";
+
 export const ADD_ORDER = 'ADD_ORDER';
+export const SET_ORDERS = 'SET_ORDERS';
+
+export const fetchOrders = () => {
+    return async dispatch => {
+        try {
+        const response = await fetch('https://pilot-shop-fa4d0-default-rtdb.firebaseio.com/orders/u1.json', {
+                method: 'GET'
+            });
+
+            if (!response.ok) {
+                throw new Error('Algo deu errado!');
+            }
+    
+            const resData = await response.json();
+            const loadedOrders = [];
+    
+            for (const key in resData) {
+                loadedOrders.push(
+                    new Order(
+                        key, 
+                        resData[key].cartItems,
+                        resData[key].totalAmount,
+                        new Date(resData[key].date)
+                        )
+                    );
+            }
+        dispatch({type: SET_ORDERS, orders: loadedOrders});
+    } catch(err) {
+        throw err;
+    }
+    };
+};
 
 export const addOrder = (cartItems, totalAmount) => {
     return async dispatch => {
