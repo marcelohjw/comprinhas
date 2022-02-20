e621b86fbda3cce752fc16731bce21f9b240bd15
Marcelo Junior <marcelohjw@gmail.com>
2022-02-20 19:03:10 -0300

    Adding User signup

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,12 +1,75 @@
-import React from 'react';
+import React, { useReducer, useCallback } from 'react';
 import { ScrollView, Button, KeyboardAvoidingView, StyleSheet, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useDispatch } from 'react-redux';
 
 import Input from '../../components/UI/Input';
 import Card from '../../components/UI/Card';
 import Colors from '../../constants/Colors';
+import * as authActions from '../../store/actions/auth';
+
+const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE';
+
+const formReducer = (state, action) => {
+  if (action.type === FORM_INPUT_UPDATE) {
+    const updatedValues = {
+      ...state.inputValues,
+      [action.input]: action.value
+    };
+    const updatedValidities = {
+      ...state.inputValidities,
+      [action.input]: action.isValid
+    };
+    let updatedFormIsValid = true;
+    for (const key in updatedValidities) {
+      updatedFormIsValid = updatedFormIsValid && updatedValidities[key];
+    }
+    return {
+      formIsValid: updatedFormIsValid,
+      inputValidities: updatedValidities,
+      inputValues: updatedValues
+    };
+  }
+  return state;
+};
+
 
 const AuthScreen = props => {
+    const dispatch = useDispatch();
+
+    const [formState, dispatchFormState] = useReducer(formReducer, {
+        inputValues: {
+        email: '',
+        password: ''
+        },
+        inputValidities: {
+        email: false,
+        password: false
+        },
+        formIsValid: false
+    });
+
+    const signupHandler = () => {
+        dispatch(
+        authActions.signup(
+            formState.inputValues.email,
+            formState.inputValues.password
+        )
+        );
+    };
+
+    const inputChangeHandler = useCallback(
+        (inputIdentifier, inputValue, inputValidity) => {
+        dispatchFormState({
+            type: FORM_INPUT_UPDATE,
+            value: inputValue,
+            isValid: inputValidity,
+            input: inputIdentifier
+        });
+        },
+        [dispatchFormState]
+    );
+
     return (
         <KeyboardAvoidingView
             behavior="padding" 
@@ -23,8 +86,8 @@ const AuthScreen = props => {
                             required
                             email
                             autoCapitalize='none'
-                            errorMessage="Coloque um email válido."
-                            onInputChange={() => {}}
+                            errorText="Coloque um email válido."
+                            onInputChange={inputChangeHandler}
                             initialValue='' 
                         />
                         <Input 
@@ -34,15 +97,23 @@ const AuthScreen = props => {
                             secureTextEntry
                             required
                             minLength={5}
-                            errorMessage="Coloque uma senha válida."
-                            onInputChange={() => {}}
+                            errorText="Coloque uma senha válida."
+                            onInputChange={inputChangeHandler}
                             initialValue='' 
                         />
                         <View style={styles.buttonContainer}>
-                            <Button title='Login' color={Colors.primary} onPress={() => {}}/>
+                            <Button
+                                title="Login"
+                                color={Colors.primary}
+                                onPress={signupHandler}
+                            />
                         </View>
                         <View style={styles.buttonContainer}>
-                            <Button title='Registrar' color={Colors.secondary} onPress={() => {}}/>
+                            <Button
+                                title="Registrar"
+                                color={Colors.secondary}
+                                onPress={() => {}}
+                            />
                         </View>
                     </ScrollView>
                 </Card>
@@ -69,9 +140,6 @@ const styles = StyleSheet.create({
         maxWidth: 400,
         maxHeight: 400,
         padding: 20
-    },
-    buttonContainer: {
-        marginTop: 10
     }
 });
 
---
+++ store/actions/auth.js
@@ -0,0 +1,26 @@
+export const SIGNUP = 'SIGNUP'
+
+export const signup = (email, password) => {
+    return async dispatch => {
+        const response = await fetch(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBGiVKUJMXgC4OXkTo802x0Jtt7LEaQieE'
+            , {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                    returnSecureToken: true
+                })
+            });
+        if (!response.ok) {
+            throw new Error('Algo deu errado!');
+        }
+
+        const resData = await response.json();
+        console.log(resData);
+        dispatch({ type: SIGNUP });
+    };
+};
